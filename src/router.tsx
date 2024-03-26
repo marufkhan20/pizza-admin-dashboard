5940633b4078bda6b9db0ffb6856aa1889537633
developmentd420 <development.d420@gmail.com>
2024-03-26 13:10:24 +0600

    Login component test

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/login/login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/auth/login",
+    element: <Login />,
   },
 ]);
