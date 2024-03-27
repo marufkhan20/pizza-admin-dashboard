429908e227cc86d847b779e162f312286b98b4da
developmentd420 <development.d420@gmail.com>
2024-03-27 12:58:04 +0600

    Persist auth on refresh

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { getSelf } from "../http/api";
+import { useAuthStore } from "../store";
+
+const getSelfData = async () => {
+  const { data } = await getSelf();
+  return data;
+};
+
+const Root = () => {
+  const { setUser } = useAuthStore();
+
+  // get self data
+  const { data, isPending } = useQuery({
+    queryKey: ["self"],
+    queryFn: getSelfData,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, setUser]);
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  return <Outlet />;
+};
+
+export default Root;
--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./layouts/Dashboard";
 import NonAuth from "./layouts/NonAuth";
+import Root from "./layouts/Root";
 import Home from "./pages/Home";
 import Login from "./pages/login/login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <Root />,
     children: [
       {
         path: "",
-        element: <Home />,
+        element: <Dashboard />,
+        children: [
+          {
+            path: "",
+            element: <Home />,
+          },
+        ],
       },
-    ],
-  },
-  {
-    path: "/auth",
-    element: <NonAuth />,
-    children: [
       {
-        path: "login",
-        element: <Login />,
+        path: "auth",
+        element: <NonAuth />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+        ],
       },
     ],
   },
