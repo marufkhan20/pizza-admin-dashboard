dc84266c1aa21e5e4fd1a4b33c81f2ea682c34d6
developmentd420 <development.d420@gmail.com>
2024-03-28 15:16:10 +0600

    Add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,14 +1,87 @@
-import { Navigate, Outlet } from "react-router-dom";
+import Icon from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import { useState } from "react";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
+import BasketIcon from "../components/icons/BasketIcon";
+import { foodIcon } from "../components/icons/FoodIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+import Home from "../components/icons/Home";
+import Logo from "../components/icons/Logo";
+import UserIcon from "../components/icons/UserIcon";
 import { useAuthStore } from "../store";
 
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+  {
+    key: "/users",
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to="/users">Users</NavLink>,
+  },
+  {
+    key: "/restaurants",
+    icon: <Icon component={foodIcon} />,
+    label: <NavLink to="/restaurants">Restaurants</NavLink>,
+  },
+  {
+    key: "/products",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+];
+
 const Dashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   const { user } = useAuthStore();
 
   if (!user) {
     return <Navigate to="/auth/login" replace />;
   }
 
-  return <Outlet />;
+  return (
+    <div>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          theme="light"
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Mernspace Pizza Shop</Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
 };
 
 export default Dashboard;
